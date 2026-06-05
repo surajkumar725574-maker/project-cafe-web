39f82d549d4fc4f1a72764641fab36d82c5e3a54
Suraj Kumar <surajkumar725574@gmail.>
2026-06-05 21:16:17 +0530

    Add deployment port

--- server.js
+++ server.js
@@ -652,6 +652,8 @@ res.send({
 // SERVER START
 // ===============================
 
-app.listen(3000, () => {
-    console.log("Server running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log("Server running on port " + PORT);
 });
