8a3e175354c54c5d12e4d2ec417873012c822e8c
Suraj Kumar <surajkumar725574@gmail.>
2026-06-08 06:44:08 +0530

    schema

--- backend/models/Order.js
+++ backend/models/Order.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose");
 const orderSchema = new mongoose.Schema({
     orderNumber:{
         type:Number,
+        required:true,
         unique:true,
-        required:true
 
 
     },
