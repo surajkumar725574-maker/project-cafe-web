27aa3d754870cce0fd987fc2e7c1f3885b05dbf9
Suraj Kumar <surajkumar725574@gmail.>
2026-06-25 14:57:25 +0530

    testing successfull

--- backend/server.js
+++ backend/server.js
@@ -54,7 +54,7 @@ function verifyAdmin(req, res, next) {
     
 
     const authHeader=req.headers.authorization;
-    console.log(req.headers.authorization);
+    
     if(!authHeader){
         return res.status(401).send({
    message:"Unauthorized"
